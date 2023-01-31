8809719a82026a9004abf6251b52c25c6aad3a45
Adrián García Mollá <113519735+Agarmol@users.noreply.github.com>
2023-01-31 12:04:26 +0100

    sum_abV2.cc

---
+++ Sum/sum_abV2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int Sumv3(int a, int b){
+    int sum = 0;
+    for(int i= a; i <= b; i++){
+        sum = sum+i;
+    }
+    return sum;
+}
+
+int main()
+{
+    int a, b, sum= 0;
+    do{
+        std::cout << "Enter a number higher than 0 for a: ";
+        std::cin >> a;
+    }
+    while(a < 1);
+    std::cout<<"Enter a number b: ";
+    std::cin >> b;
+    sum = Sumv3(a, b);
+    std::cout<< "La suma entre " << a << " y " << b << " es :" << sum;
+    
+    return 0;
+}
