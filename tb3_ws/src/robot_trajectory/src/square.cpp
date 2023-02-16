e0c948e7bc511135943ff0abd58f9e02cbf98b6f
Agarmol <adripromlg@gmail.com>
2023-02-16 13:19:15 +0100

    Move forward during one meter

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -10,13 +10,21 @@ int main(int argc, char * argv[])
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist message;
-  rclcpp::WallRate loop_rate(500ms);
-
-  while (rclcpp::ok()) {
+  rclcpp::WallRate loop_rate(10ms);
+  
+  int i= 10, n= 1000;
+  
+  while (rclcpp::ok() && (i<n)) {
+    i++;
+    message.linear.x = 0.1;
+    message.angular.z = 0;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
   }
+  
+  message.linear.x = 0;
+  publisher->publish(message);
   rclcpp::shutdown();
   return 0;
 }
