8bf4f394b16252e067c07ef8a18349b35a99cea9
Agarmol <adripromlg@gmail.com>
2023-02-20 23:49:38 +0100

    mode v2 count whichever number

--- stats/mode.cpp
+++ stats/mode.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <map>
+using namespace std;
+
 int main(int argc, char** argv) {
   doubble m=0, s=0;
   int n=0, element;
@@ -8,17 +11,21 @@ int main(int argc, char** argv) {
   while (not std::cin.eof()) {
     elements.push_back(element);
     n += 1;
-    std::cin >> element;
+    cin >> element;
     }
     
-      //Count first number
-    int x;
+    //Count number of elements equal to whichever
+    map<double,int> dicc;
     for(int i=0; i<elements.size();i++){
-        if(elements[0]==elements[i]){
-            x++;
+      int x=0;
+        for(int j=0; j<elements.size();j++){
+            if(elements[i]==elements[j]){
+                x++;
+            }
+            dicc[i]=x;
         }
     }
 
-    std::cout << "Mode: " << m << std::endl;
+    cout << "Mode: " << m << std::endl;
     return 0;
 }
