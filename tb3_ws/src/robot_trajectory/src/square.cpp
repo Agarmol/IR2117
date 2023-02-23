af42026cfc592499d1d533a87c4ad31e7d9a06d8
Agarmol <adripromlg@gmail.com>
2023-02-23 12:48:13 +0100

    define two ROS parameters

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -7,17 +7,20 @@ using namespace std::chrono_literals;
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("publisher");
+  auto node = rclcpp::Node::make_shared("square");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  node->declare_parameter("linear_speed", 0.1);
+  node->declare_parameter("angular_speed", 3.1416/20);
+  
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(10ms);
-  
-  int i= 10, n= 1000;
-for(int j=0; j<4; j++){  
+ double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
+ double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
+for(int j=0; j<4; j++){
+   int i= 10, n= 1000; 
    while (rclcpp::ok() && (i<n)) {
      i++;
-     message.linear.x = 0.1;
-     message.angular.z = 0;
+     message.linear.x = linear_speed;
      publisher->publish(message);
      rclcpp::spin_some(node);
      loop_rate.sleep();
@@ -31,8 +34,7 @@ for(int j=0; j<4; j++){
   
    while (rclcpp::ok() && (i<n)) {
      i++;
-     message.linear.x = 0;
-     message.angular.z = 0.1;
+     message.angular.z = angular_speed;
      publisher->publish(message);
      rclcpp::spin_some(node);
      loop_rate.sleep();
