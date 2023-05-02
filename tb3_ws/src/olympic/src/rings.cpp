999588379aa70664de2f3fc59f433c6ad16f6e03
Agarmol <adripromlg@gmail.com>
2023-05-02 12:50:08 +0200

    v4 draw_circle

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -16,17 +16,24 @@ int main(int argc, char * argv[])
  geometry_msgs::msg::Twist message;
  auto publish_count = 0;
  rclcpp::WallRate loop_rate(500ms);
+ 
+double radius_length = node->get_parameter("radius_length").get_parameter_value().get<double>();
 
- while (rclcpp::ok()) {
-   while (rclcpp::ok()) {
-   message.linear.x = 1.0;
-   ...
+double circle= 2* M_PI;
+
+int iterations = static_cast<int>(circle/0.500);
+
+ for (int i = 0; i < iterations; i++) {
+   message.linear.x = 1;
+   message.angular.z = 2*M_PI / circle;
    publisher->publish(message);
    rclcpp::spin_some(node);
    loop_rate.sleep();
  }
-
- }
+  message.linear.x = 0;
+  message.angular.z = 0;
+  publisher->publish(message);
+   
  rclcpp::shutdown();
  return 0;
 }
