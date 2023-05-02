de03df40c4b3d3a2f076b404aef846e8279efed9
Agarmol <adripromlg@gmail.com>
2023-05-02 12:20:32 +0200

    change the topic rings

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include <cmath>
 
 using namespace std::chrono_literals;
 
@@ -8,16 +10,22 @@ int main(int argc, char * argv[])
 {
  rclcpp::init(argc, argv);
  auto node = rclcpp::Node::make_shared("rings");
- auto publisher = node->create_publisher<std_msgs::msg::String>("/turtle1/cmd_vel", 10);
- std_msgs::msg::String message;
+ auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+
+ node->declare_parameter("radius_length", 1.0);
+ geometry_msgs::msg::Twist message;
  auto publish_count = 0;
  rclcpp::WallRate loop_rate(500ms);
 
  while (rclcpp::ok()) {
-   message.data = "Hello, world! " + std::to_string(publish_count++);
+   while (rclcpp::ok()) {
+   message.linear.x = 1.0;
+   ...
    publisher->publish(message);
    rclcpp::spin_some(node);
    loop_rate.sleep();
+ }
+
  }
  rclcpp::shutdown();
  return 0;
