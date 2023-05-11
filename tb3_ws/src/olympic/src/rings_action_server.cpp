c57fd8d3831a7dc6bce514ce462639582d798658
Agarmol <adripromlg@gmail.com>
2023-05-11 13:11:07 +0200

    v3 change to rings

--- tb3_ws/src/olympic/src/rings_action_server.cpp
+++ tb3_ws/src/olympic/src/rings_action_server.cpp
@@ -1,6 +1,6 @@
 #include <inttypes.h>
 #include <memory>
-#include "action_tutorials_interfaces/action/fibonacci.hpp"
+#include "olympics_interfaces/action/rings.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
@@ -20,7 +20,7 @@ using GoalHandleRings=
 }
 
 rclcpp_action::CancelResponse handle_cancel(
-  const std::shared_ptr<GoalHandleFibonacci> goal_handle)
+  const std::shared_ptr<GoalHandleRings> goal_handle)
 {
   RCLCPP_INFO(rclcpp::get_logger("server"), 
     "Got request to cancel goal");
@@ -31,23 +31,23 @@ rclcpp_action::CancelResponse handle_cancel(
 void execute(const std::shared_ptr<GoalHandleRings>);
 
 void handle_accepted(
-  const std::shared_ptr<GoalHandleFibonacci> goal_handle)
+  const std::shared_ptr<GoalHandleRings> goal_handle)
 {
   std::thread{execute, goal_handle}.detach();
 }
 
 void execute(
-  const std::shared_ptr<GoalHandleFibonacci> goal_handle)
+  const std::shared_ptr<GoalHandleRings> goal_handle)
 {
   RCLCPP_INFO(rclcpp::get_logger("server"), 
     "Executing goal");
   rclcpp::Rate loop_rate(1);
   const auto goal = goal_handle->get_goal();
-  auto feedback = std::make_shared<Fibonacci::Feedback>();
+  auto feedback = std::make_shared<Rings::Feedback>();
   auto & sequence = feedback->partial_sequence;
   sequence.push_back(0);
   sequence.push_back(1);
-  auto result = std::make_shared<Fibonacci::Result>();
+  auto result = std::make_shared<Rings::Result>();
 
 for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i) {
     if (goal_handle->is_canceling()) {
@@ -77,7 +77,7 @@ int main(int argc, char ** argv)
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("action_server");
   auto action_server = 
-    rclcpp_action::create_server<Fibonacci>(node,
+    rclcpp_action::create_server<Rings>(node,
       "fibonacci",
       handle_goal,
       handle_cancel,
