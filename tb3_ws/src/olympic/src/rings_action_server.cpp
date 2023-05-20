6ce88348473104d386f7e4141e5867b9cd3cb76c
Agarmol <adripromlg@gmail.com>
2023-05-20 20:17:44 +0200

    v4 actions

--- tb3_ws/src/olympic/src/rings_action_server.cpp
+++ tb3_ws/src/olympic/src/rings_action_server.cpp
@@ -1,20 +1,17 @@
 #include <inttypes.h>
 #include <memory>
-#include "olympics_interfaces/action/rings.hpp"
+#include "olympic_interfaces/action/rings.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
-using Rings = 
-  action_tutorials_interfaces::action::Rings;
+using Rings = olympic_interfaces::action::Rings;
+using GoalHandleRings = rclcpp_action::ServerGoalHandle<Rings>;
 
-using GoalHandleRings= 
-  rclcpp_action::ServerGoalHandle<Rings>;
-  rclcpp_action::GoalResponse handle_goal(
-  const rclcpp_action::GoalUUID & uuid, 
+rclcpp_action::GoalResponse handle_goal(
+  const rclcpp_action::GoalUUID& uuid,
   std::shared_ptr<const Rings::Goal> goal)
 {
-  RCLCPP_INFO(rclcpp::get_logger("server"), 
-    "Got goal request with order %d", goal->order);
+  RCLCPP_INFO(rclcpp::get_logger("server"), "Got goal request with radius %" PRId64, goal->radius);
   (void)uuid;
   return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
 }
@@ -22,68 +19,60 @@ using GoalHandleRings=
 rclcpp_action::CancelResponse handle_cancel(
   const std::shared_ptr<GoalHandleRings> goal_handle)
 {
-  RCLCPP_INFO(rclcpp::get_logger("server"), 
-    "Got request to cancel goal");
+  RCLCPP_INFO(rclcpp::get_logger("server"), "Got request to cancel goal");
   (void)goal_handle;
   return rclcpp_action::CancelResponse::ACCEPT;
 }
 
-void execute(const std::shared_ptr<GoalHandleRings>);
-
-void handle_accepted(
-  const std::shared_ptr<GoalHandleRings> goal_handle)
+void execute(const std::shared_ptr<GoalHandleRings> goal_handle)
 {
-  std::thread{execute, goal_handle}.detach();
-}
-
-void execute(
-  const std::shared_ptr<GoalHandleRings> goal_handle)
-{
-  RCLCPP_INFO(rclcpp::get_logger("server"), 
-    "Executing goal");
+  RCLCPP_INFO(rclcpp::get_logger("server"), "Executing goal");
   rclcpp::Rate loop_rate(1);
   const auto goal = goal_handle->get_goal();
   auto feedback = std::make_shared<Rings::Feedback>();
-  auto & sequence = feedback->partial_sequence;
+  auto& sequence = feedback->partial_sequence;
   sequence.push_back(0);
   sequence.push_back(1);
   auto result = std::make_shared<Rings::Result>();
 
-for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i) {
+  for (int i = 1; (i < goal->radius) && rclcpp::ok(); ++i) {
     if (goal_handle->is_canceling()) {
       result->sequence = sequence;
       goal_handle->canceled(result);
-      RCLCPP_INFO(rclcpp::get_logger("server"), 
-        "Goal Canceled");
+      RCLCPP_INFO(rclcpp::get_logger("server"), "Goal Canceled");
       return;
     }
-    sequence.push_back(sequence[i] + sequence[i - 1]);
+    // Your code for drawing circles here
     goal_handle->publish_feedback(feedback);
-    RCLCPP_INFO(rclcpp::get_logger("server"), 
-      "Publish Feedback");
+    RCLCPP_INFO(rclcpp::get_logger("server"), "Publish Feedback");
     loop_rate.sleep();
   }
 
-if (rclcpp::ok()) {
+  if (rclcpp::ok()) {
     result->sequence = sequence;
     goal_handle->succeed(result);
-    RCLCPP_INFO(rclcpp::get_logger("server"), 
-      "Goal Succeeded");
+    RCLCPP_INFO(rclcpp::get_logger("server"), "Goal Succeeded");
   }
 }
 
-int main(int argc, char ** argv)
+void handle_accepted(const std::shared_ptr<GoalHandleRings> goal_handle)
 {
-  rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("action_server");
-  auto action_server = 
-    rclcpp_action::create_server<Rings>(node,
-      "fibonacci",
-      handle_goal,
-      handle_cancel,
-      handle_accepted);
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
+  std::thread{execute, goal_handle}.detach();
 }
 
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("rings_action_server");
+  auto action_server = rclcpp_action::create_server<Rings>(
+    node,
+    "rings",
+    handle_goal,
+    handle_cancel,
+    handle_accepted);
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+  return 0;
+}
